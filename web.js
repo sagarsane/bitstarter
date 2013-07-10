8f7207c180cb3c85adb45a357c0afd61a82f0909
Sagar Sane <sagar2217@gmail.com>
2013-07-10 00:00:09 -0400

    Modified web.js

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/', function(request, response){ 
+  var file = fs.readFileSync("index.html");
+  response.send(file.toString());
 });
 
 var port = process.env.PORT || 5000;

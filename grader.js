5530826b43f78d8c4a20ffc17bc812122c0727f0
Sagar Sane <sagar2217@gmail.com>
2013-07-10 00:58:31 -0400

    Modified grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://secure-harbor-3236.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,13 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+
+var assertUrlExists = function(inUrl) {
+	restler.get(inUrl).on('complete', function(result, response){
+		return result;
+	}); 
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,6 +74,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u --url <site_utl>', 'Path to the site', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
